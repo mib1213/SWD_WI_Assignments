e525e42b340b054095332a91926b28c443f500d2
omer <mirzaomer@outlook.de>
2024-12-27 03:35:48 +0100

    ignoring class and seq files

---
+++ Assignment_09/galgenmännchen/check_char.py
@@ -0,0 +1,10 @@
+# Aufgabe 2
+
+def check_char(word: str, chars: list[str]) -> str:
+    solution = [' _ '] * len(word)
+    for char in chars:
+        if char in word:
+            idxs = [idx for idx, char_ in enumerate(word) if char_ == char]
+            for idx in idxs:
+                solution[idx] = char
+    return ''.join(solution)
